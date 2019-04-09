922e0b7391a78aa2f68cc7f5fcb77965cd2c6463
Grant Miiller <grant@m2iller.com>
2019-04-09 08:47:23 -0400

    Did some form handling

--- app/assets/form.js
+++ app/assets/form.js
@@ -1,1 +1,43 @@
-console.log("I am here");
+
+function removeModal() {
+  const modal = document.getElementById("successModal");
+  if (modal) {
+    modal.parentElement.removeChild(modal);
+    window.removeEventListener("click", removeModal);
+  }
+}
+
+function createModal() {
+  const docFrag = document.createDocumentFragment();
+  const outerDiv = document.createElement('div');
+  const innerDiv = document.createElement('div');
+  const innerText = document.createElement('span');
+  const closeText = document.createElement('span');
+
+  outerDiv.id = "successModal";
+  outerDiv.className = "outer-modal";
+
+  innerDiv.className = "inner-modal";
+
+  innerText.className = "inner-modal-text";
+  innerText.textContent = "Thank you for your purchase!";
+
+  closeText.className = "close-modal-text";
+  closeText.textContent = "(Click anywhere to close)";
+
+  innerDiv.appendChild(closeText);
+  innerDiv.appendChild(innerText);
+  outerDiv.appendChild(innerDiv);
+  docFrag.appendChild(outerDiv);
+  document.body.appendChild(docFrag);
+
+  window.addEventListener("click", removeModal);
+}
+
+const ccForm = document.getElementById('ccForm');
+
+ccForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createModal();
+});
+
--- app/template.php
+++ app/template.php
@@ -2,11 +2,17 @@
 <html>
 <head>
   <title>Wizard Sled Store</title>
-  <script type="text/javascript" src="assets/form.js"></script>
 </head>
 <body>
   <h1>Welcome to the Wizard Sled Store!</h1>
   <h2>Here are your purchases today <?php print $user->username ?></h2>
   <p>Add a new line</p>
+  <form id="ccForm">
+
+    <input type="text" name="name" />
+    <input type="text" name="cc-num" placeholder="xxxx-xxxx-xxxx-xxxx" />
+    <input type="submit" value="Submit" />
+  </form>
+  <script type="text/javascript" src="assets/form.js"></script>
 </body>
 </html>
